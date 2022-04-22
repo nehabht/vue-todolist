a189ec29398bd5f88e1a43b5ba15908ec7080ee8
nehabht <ne.bhatti2@icloud.com>
2022-04-22 15:54:50 +0200

    add milestone2

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,8 +21,13 @@ const app = new Vue({
             },
     
         ]
-            
-        
+    },
+
+    methods:{
+        removeTask(indice){
+            console.log("rimosso", indice)
+            this.tasks.splice(indice, 1)
+        }
     }
         
     
@@ -33,3 +38,6 @@ const app = new Vue({
 Stampare all'interno di una lista, un item per ogni todo. 
 Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato. */
 
+/* MILESTONE 2
+ Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, 
+ il todo viene rimosso dalla lista. */
