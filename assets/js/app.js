40ea10d3c556d6bb6cc00d41b624aef5c5c74c4d
nehabht <ne.bhatti2@icloud.com>
2022-04-22 18:42:29 +0200

    add milestone3

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,9 @@ const app = new Vue({
     el:"#app",
 
     data:{
+
+        newTask: '',
+
         tasks:[
             {
                 text: "pranzo",
@@ -26,7 +29,24 @@ const app = new Vue({
     methods:{
         removeTask(indice){
             console.log("rimosso", indice)
+            //con splice tolgo l'elemento dall'array
             this.tasks.splice(indice, 1)
+        },
+
+        addTask(){
+
+            let newAdd = {
+                text : this.newTask,
+                done : false,
+            }
+            //con unshift immetto l'elemento nell'array
+            this.tasks.unshift(newAdd)
+
+            //svuoto il campo input
+            this.newTask = '';
+
+
+            
         }
     }
         
@@ -41,3 +61,10 @@ Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 /* MILESTONE 2
  Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, 
  il todo viene rimosso dalla lista. */
+
+
+/*  MILESTONE 3
+ Predisporre un campo di input testuale 
+ e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto 
+ e utilizzato per creare un nuovo todo, 
+ che quindi viene aggiunto alla lista dei todo esistenti. */
